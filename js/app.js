450100a71a598c9731faf33bfecfa40a5701b50e
Jayesh Choudhary <jayesh.choudhary@spaceuptech.com>
2017-10-04 18:51:53 +0530

    Minor fixes

--- js/app.js
+++ js/app.js
@@ -1,12 +1,16 @@
 $(document).ready(function() {
   $("#navbar").hide()
   $(".button-collapse").sideNav();
-  $("#contact-us-btn").click(function() {
+  $("#contact-us-btn1").click(function() {
     $('html, body').animate({
       scrollTop: $("#contact-us").offset().top
     }, 2000);
   });
-
+  $("#contact-us-btn2").click(function() {
+    $('html, body').animate({
+      scrollTop: $("#contact-us").offset().top
+    }, 2000);
+  });
   $(window).scroll(function() {
     var scroll = $(window).scrollTop();
     if (scroll >= 300) $("#navbar").show()
