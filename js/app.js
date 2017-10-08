bfcb9612eb383ed5bb7eee2324dfc65fd94653e1
Jayesh Choudhary <jayesh.choudhary@spaceuptech.com>
2017-10-08 15:29:44 +0530

    Second Release

--- js/app.js
+++ js/app.js
@@ -1,19 +1,19 @@
 $(document).ready(function() {
-  $("#navbar").hide()
+  var navClosed = true
   $(".button-collapse").sideNav();
-  $("#contact-us-btn1").click(function() {
+  $("#contact-us-btn2, #contact-us-btn1").click(function() {
     $('html, body').animate({
       scrollTop: $("#contact-us").offset().top
-    }, 2000);
-  });
-  $("#contact-us-btn2").click(function() {
-    $('html, body').animate({
-      scrollTop: $("#contact-us").offset().top
-    }, 2000);
+    }, 1000);
   });
   $(window).scroll(function() {
     var scroll = $(window).scrollTop();
-    if (scroll >= 300) $("#navbar").show()
-    else $("#navbar").hide()
+    if (scroll >= 100 && navClosed) {
+      $("#navbar").removeClass('close')
+      navClosed = false
+    } else if (scroll < 100 && !navClosed) {
+      $("#navbar").addClass('close')
+      navClosed = true
+    }
   });
 });
